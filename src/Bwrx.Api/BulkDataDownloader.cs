dd7eafab555935aad56b2346f669f437ac2e449b
Paul Mooney <daishisystems@users.noreply.github.com>
2019-08-22 14:00:13 +0100

    Added BulkDataDownloader to get table record-counts

---
+++ src/Bwrx.Api/BulkDataDownloader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Bwrx.Api
+{
+    public class BulkDataDownloader
+    {
+        public async Task<RecordCount> GetRecordCount(HttpClient httpClient, string requestUri)
+        {
+            if (httpClient == null) throw new ArgumentNullException(nameof(httpClient));
+            if (string.IsNullOrEmpty(requestUri)) throw new ArgumentNullException(nameof(requestUri));
+
+            try
+            {
+                var httpResponse = await httpClient.GetStringAsync(requestUri);
+                return JsonConvert.DeserializeObject<RecordCount>(httpResponse);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Unable to get record-count", e);
+            }
+        }
+    }
+
+    public class RecordCount
+    {
+        public int Total { get; set; }
+    }
+}
---
+++ tests/Bwrx.Tests/Integration/BulkRecordDownloaderTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using Bwrx.Api;
+using Xunit;
+
+namespace Bwrx.Tests.Integration
+{
+    public class BulkRecordDownloaderTests
+    {
+        [Fact]
+        public void CanGetRecordCount()
+        {
+            var bulkDataDownloader = new BulkDataDownloader();
+
+            using (var httpClient = new HttpClient())
+            {
+                var recordCountMeta = bulkDataDownloader.GetRecordCount(httpClient,
+                    "https://us-central1-bwrx-dev.cloudfunctions.net/record-count-test-0").Result;
+                Assert.Equal(8, recordCountMeta.Total);
+            }
+        }
+    }
+}
